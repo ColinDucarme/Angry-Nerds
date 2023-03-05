f6f7de61efbddc2b599cee96fe1c15f2cba54e65
ColinDucarme <99869746+ColinDucarme@users.noreply.github.com>
2023-03-05 14:25:10 +0100

    Add files via upload

--- Obstacles.py
+++ Obstacles.py
@@ -15,10 +15,7 @@ class Enemy(pygame.sprite.Sprite):
             ratio_width = (obs.Xmax-obs.Xmin)/self.surf.get_width()
             ratio_height = (obs.Ymax-obs.Ymin)/self.surf.get_height()*ratio
             self.surf = pygame.transform.scale(self.surf, (self.WIDTH/3, self.WIDTH/3*ratio))
-            print(self.surf.get_rect().left)
-            print(self.surf.get_rect().top)
-            self.rect = pygame.Rect(self.surf.get_rect().left+obs.Xmin/origin_width*self.WIDTH/3+1, self.surf.get_rect().top+obs.Ymin/origin_height*self.WIDTH/3*ratio+1, self.WIDTH/3*ratio_width-2, self.WIDTH/3*ratio_height-2)
-            self.surf = self.surf.subsurface(self.rect)
+            self.rect = pygame.Rect(self.surf.get_rect().left+obs.Xmin/origin_width*self.WIDTH/3, self.surf.get_rect().top+obs.Ymin/origin_height*self.WIDTH/3*ratio, self.WIDTH/3*ratio_width, self.WIDTH/3*ratio_height)
             self.rect = self.surf.get_rect(center=(self.WIDTH / 3 * (obs.col - 1) + self.WIDTH / 6, self.WIDTH / 6))
             self.speed = speed
         else :
